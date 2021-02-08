8566095c2fafde101762cac5a285fd5c4c11290f
chajinjoo <chajanee@gmail.com>
2021-02-08 23:13:42 +0900

    커스텀 테이블뷰 셀 구현

---
+++ iOS/BountyList/BountyList/BountyViewController.swift
@@ -0,0 +1,52 @@
+//
+//  BountyViewController.swift
+//  BountyList
+//
+//  Created by JINJOO on 2021/02/08.
+//
+
+import UIKit
+
+class BountyViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    // 더미 데이터 세팅
+    let nameList = ["brook", "chopper", "franky", "luffy", "nami", "robin", "sanji", "zoro"]
+    let bountyList = [33000000, 50, 44000000, 300000000, 16000000, 80000000, 77000000, 120000000]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    // UITableViewDataSource
+    // 테이블뷰 셀이 몇개임?
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return bountyList.count // 데이터의 갯수
+    }
+    
+    // 테이블뷰 어떻게 보여줄까?
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? ListCell else {
+            return UITableViewCell()
+        }
+        
+        return cell
+    }
+
+    // UITableViewDelegate
+    // 테이블 뷰 클릭하면 어떻게 할까?
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("--> \(indexPath.row)")
+    }
+
+}
+
+// 커스텀 셀
+class ListCell: UITableViewCell {
+    @IBOutlet weak var imgView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var bountyLabel: UILabel!
+}

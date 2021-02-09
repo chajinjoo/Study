5a0cb5802268f2a3e5623bd64fdb86760b7a26ce
chajinjoo <chajanee@gmail.com>
2021-02-09 22:31:40 +0900

    MVVM으로 리팩토링

--- iOS/BountyList/BountyList/BountyViewController.swift
+++ iOS/BountyList/BountyList/BountyViewController.swift
@@ -8,10 +8,20 @@
 import UIKit
 
 class BountyViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
-
+    // MVVM
+    
+    // Model
+    // BountyInfo 만들기
+    
+    // View
+    // ListCell에 필요한 정보를 ViewModel한테 받아서 뷰 업데이트 하기
+    
+    // ViewModel
+    // BountyViewModel을 만들고 뷰레이어에서 필요한 메서드 만들기
+    // 모델 갖고있기, BountyInfo들...
+    
     // 더미 데이터 세팅
-    let nameList = ["brook", "chopper", "franky", "luffy", "nami", "robin", "sanji", "zoro"]
-    let bountyList = [33000000, 50, 44000000, 300000000, 16000000, 80000000, 77000000, 120000000]
+    let viewModel = BountyViewModel()
     
     // 연결한 세그웨이를 수행하기 직전에 데이터를 넘겨주자!
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -20,8 +30,9 @@ class BountyViewController: UIViewController, UITableViewDataSource, UITableView
             let vc = segue.destination as? DetailViewController
             
             if let index = sender as? Int {
-                vc?.name = nameList[index]
-                vc?.bounty = bountyList[index]
+                let bountyInfo = viewModel.bountyInfo(at: index)
+                
+                vc?.viewModel.update(model: bountyInfo)
             }
         }
     }
@@ -32,11 +43,10 @@ class BountyViewController: UIViewController, UITableViewDataSource, UITableView
         // Do any additional setup after loading the view.
     }
     
-    
     // UITableViewDataSource
     // 테이블뷰 셀이 몇개임?
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return bountyList.count // 데이터의 갯수
+        return viewModel.nimOfBountyInfoList // 데이터의 갯수
     }
     
     // 테이블뷰 어떻게 보여줄까?
@@ -46,11 +56,9 @@ class BountyViewController: UIViewController, UITableViewDataSource, UITableView
             return UITableViewCell()
         }
         
-        let img = UIImage(named: "\(nameList[indexPath.row]).jpg")
-        cell.imgView.image = img
-        cell.nameLabel.text = nameList[indexPath.row]
-        cell.bountyLabel.text = "\(bountyList[indexPath.row])"
-        
+        let bountyInfo = viewModel.bountyInfo(at: indexPath.row)
+        cell.update(info: bountyInfo)
+
         return cell
     }
 
@@ -62,7 +70,6 @@ class BountyViewController: UIViewController, UITableViewDataSource, UITableView
         // sender 파라미터에 정보를 넣어서 보냄
         performSegue(withIdentifier: "showDetail", sender: indexPath.row)
     }
-
 }
 
 // 커스텀 셀
@@ -70,4 +77,31 @@ class ListCell: UITableViewCell {
     @IBOutlet weak var imgView: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var bountyLabel: UILabel!
+    
+    func update(info: BountyInfo) {
+        imgView.image = info.image
+        nameLabel.text = info.name
+        bountyLabel.text = "\(info.bounty)"
+    }
+}
+
+class BountyViewModel {
+    let bountyInfoList: [BountyInfo] = [
+        BountyInfo(name: "brook", bounty: 33000000),
+        BountyInfo(name: "chopper", bounty: 50),
+        BountyInfo(name: "franky", bounty: 44000000),
+        BountyInfo(name: "luffy", bounty: 300000000),
+        BountyInfo(name: "nami", bounty: 16000000),
+        BountyInfo(name: "robin", bounty: 80000000),
+        BountyInfo(name: "sanji", bounty: 77000000),
+        BountyInfo(name: "zoro", bounty: 120000000)
+    ]
+    
+    var nimOfBountyInfoList: Int {
+        return bountyInfoList.count
+    }
+    
+    func bountyInfo(at index: Int) -> BountyInfo{
+       return bountyInfoList[index]
+    }
 }
--- iOS/BountyList/BountyList/DetailViewController.swift
+++ iOS/BountyList/BountyList/DetailViewController.swift
@@ -8,14 +8,25 @@
 import UIKit
 
 class DetailViewController: UIViewController {
+    // MVVM
+    
+    // Model
+    // BountyInfo 만들기
+    
+    // View
+    // imgView, nameLabel, bountyLabel
+    // view들은 viewModel을 통해서 구성되기
+    
+    // ViewModel
+    // DetailViewModel을 만들고 뷰레이어에서 필요한 메서드 만들기
+    // 모델 갖고있기, BountyInfo들...
     
     @IBOutlet weak var imgView: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var bountyLabel: UILabel!
     
     // 데이터에 필요한 프로퍼티 추가
-    var name: String?
-    var bounty: Int?
+    let viewModel = DetailViewModel()
 
     // 화면에 띄우기 직전에 호출되는 메서드
     override func viewDidLoad() {
@@ -25,11 +36,10 @@ class DetailViewController: UIViewController {
     
     // UI 업데이트
     func updateUI() {
-        if let name = self.name, let bounty = self.bounty {
-            let img = UIImage(named: "\(name).jpg")
-            imgView.image = img
-            nameLabel.text = name
-            bountyLabel.text = "\(bounty)"
+        if let bountyInfo = viewModel.bountyInfo {
+            imgView.image = bountyInfo.image
+            nameLabel.text = bountyInfo.name
+            bountyLabel.text = "\(bountyInfo.bounty)"
         }
     }
 
@@ -37,5 +47,12 @@ class DetailViewController: UIViewController {
     @IBAction func close(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
+}
 
+class DetailViewModel {
+    var bountyInfo: BountyInfo?
+    
+    func update(model: BountyInfo?) {
+        bountyInfo = model
+    }
 }
